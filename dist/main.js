c542a301b8f6f347d6af5980fd65cd4159502e31
Saber FRAJ <frejsaber@gmail.com>
2020-02-26 15:59:47 +0100

    add build

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// function hello(compiler: string) {
-//     console.log(`Hello from ${compiler}`);
-// }
-// hello('TypeScript');
 var greet_1 = require("./greet");
 console.log(greet_1.sayHello('Typescript'));
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,9 @@
-import {sayHello } from './greet'
-console.log(sayHello('Typescript'));
+import { sayHello } from './greet';
+
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    const temp =elt.innerText;
+    elt.innerText = temp+' '+sayHello(name);
+}
+
+showHello('greeting', 'TypeScript');
